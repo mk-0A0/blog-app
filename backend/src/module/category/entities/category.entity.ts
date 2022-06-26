82aef3b9a7837810a581868608883b634e6906eb
hisho <hisho.web@gmail.com>
2022-06-26 23:49:44 +0900

    🎉 entityを定義

--- backend/src/module/category/entities/category.entity.ts
+++ backend/src/module/category/entities/category.entity.ts
@@ -1,7 +1,9 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, PickType } from '@nestjs/graphql';
+import { Category as CategoryModel } from '../../../prisma/@generated/prisma-nestjs-graphql/category/category.model';
+import { Post } from '../../post/entities/post.entity';
 
 @ObjectType()
-export class Category {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class Category extends PickType(CategoryModel, ['id', 'uuid', 'name']) {
+  @Field(() => [Post], { description: 'カテゴリーに紐付いている記事' })
+  posts: Post[];
 }
--- backend/src/module/user/entities/user.entity.ts
+++ backend/src/module/user/entities/user.entity.ts
@@ -1,7 +1,9 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, PickType } from '@nestjs/graphql';
+import { User as UserModel } from '../../../prisma/@generated/prisma-nestjs-graphql/user/user.model';
+import { Post } from '../../post/entities/post.entity';
 
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class User extends PickType(UserModel, ['email', 'name', 'id', 'uuid']) {
+  @Field(() => [Post], { description: '投稿' })
+  posts: Post[];
 }
