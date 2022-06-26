4e80fa38c59f39be67592f8ac49fccd23a24f69d
hisho <hisho.web@gmail.com>
2022-06-26 16:01:43 +0900

    🎉 prismaとgraphqlとnestを紐付ける

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { PrismaService } from './prisma/prisma.service';
+import { PostModule } from './module/post/post.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    PostModule,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
