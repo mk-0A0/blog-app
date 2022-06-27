cad29a4dc4b04e49f622db6377404c55b4b3d9ce
hisho <hisho.web@gmail.com>
2022-06-28 02:01:03 +0900

    ✨ エラーを日本語化

--- backend/src/module/category/dto/create-category.input.ts
+++ backend/src/module/category/dto/create-category.input.ts
@@ -1,7 +1,15 @@
 import { Field, InputType } from '@nestjs/graphql';
+import {
+  CIsNotEmpty,
+  CIsString,
+  CLength,
+} from '../../../decorators/standardClassValidators';
 
 @InputType()
 export class CreateCategoryInput {
   @Field(() => String, { nullable: false, description: 'カテゴリー名' })
+  @CIsNotEmpty('カテゴリー名')
+  @CIsString('カテゴリー名')
+  @CLength('カテゴリー名', 1, 255)
   name!: string;
 }
--- backend/src/module/post/dto/create-post.input.ts
+++ backend/src/module/post/dto/create-post.input.ts
@@ -1,14 +1,26 @@
 import { Field, InputType } from '@nestjs/graphql';
+import {
+  CIsBoolean,
+  CIsNotEmpty,
+  CIsString,
+  CLength,
+} from '../../../decorators/standardClassValidators';
 
 @InputType()
 export class CreatePostInput {
   @Field(() => String, { nullable: false, description: '記事のタイトル' })
+  @CIsNotEmpty('記事のタイトル')
+  @CIsString('記事のタイトル')
+  @CLength('記事のタイトル', 1, 255)
   title!: string;
 
   @Field(() => String, { nullable: false, description: '記事の本文' })
+  @CIsNotEmpty('記事の本文')
+  @CIsString('記事の本文')
   content!: string;
 
   @Field(() => Boolean, { nullable: true, description: '記事を公開するか' })
+  @CIsBoolean('記事を公開するか')
   isPublished?: boolean;
 
   @Field(() => [String], {
