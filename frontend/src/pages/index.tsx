5a0e54c9a34f6d1d09009bd30bf529bbfbe5f986
momoka <moowfaw@gmail.com>
2022-07-12 20:27:21 +0900

    🎨create post's view

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,32 @@
 import type { NextPage } from 'next'
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  Textarea,
+  VStack,
+} from '@chakra-ui/react'
 
 const Home: NextPage = () => {
-  return <>aaa</>
+  return (
+    <Container maxW={500} w={'full'} mx={'auto'}>
+      <VStack gap={5}>
+        <Box w={'full'}>
+          <Heading>タイトル</Heading>
+          <Input />
+        </Box>
+        <Box w={'full'}>
+          <Heading>本文</Heading>
+          <Textarea />
+        </Box>
+        <Button w={'full'} variant={'solid'}>
+          投稿
+        </Button>
+      </VStack>
+    </Container>
+  )
 }
 
 export default Home
