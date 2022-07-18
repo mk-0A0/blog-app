41adb4e35b0ec8d83e0e870d3a138e41f98f97b2
momoka <moowfaw@gmail.com>
2022-07-18 17:02:31 +0900

    🎨add link to detail & fixed style

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,19 @@
 import { NextPage } from 'next'
 import { useQuery } from '@apollo/client'
 import { PostsDocument } from './Posts.generate.graphql'
-import { Box, Button, Container, Grid, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  HStack,
+  IconButton,
+  Spacer,
+  Text,
+} from '@chakra-ui/react'
 import Link from 'next/link'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 
 const Home: NextPage = () => {
   const { data } = useQuery(PostsDocument)
@@ -10,12 +21,25 @@ const Home: NextPage = () => {
     <Container maxW={500} w={'full'} mx={'auto'}>
       <Grid gap={5}>
         {data?.posts.map((post, index) => (
-          <Box key={`post_${index}`}>
-            <Text fontWeight={'bold'} fontSize={'lg'}>
-              {post.title}
-            </Text>
-            <Text>{post.content}</Text>
-          </Box>
+          <>
+            <HStack key={`post_${index}`}>
+              <Box>
+                <Text fontWeight={'bold'} fontSize={'lg'}>
+                  {post.title}
+                </Text>
+                <Text>{post.content}</Text>
+              </Box>
+              <Spacer />
+              <Link href={`/${post.uuid}`}>
+                <IconButton
+                  variant={'ghost'}
+                  aria-label={'button'}
+                  icon={<ChevronRightIcon />}
+                />
+              </Link>
+            </HStack>
+            <Divider />
+          </>
         ))}
       </Grid>
       <Button mt={10} w={'full'}>
