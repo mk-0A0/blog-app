ebdbae565064b322afa8526c240b32b2cc966311
momoka <moowfaw@gmail.com>
2022-07-18 18:14:00 +0900

    🎨styling postDetail

--- frontend/src/pages/[post_uuid]/index.tsx
+++ frontend/src/pages/[post_uuid]/index.tsx
@@ -1,8 +1,9 @@
-import { Container, Text } from '@chakra-ui/react'
+import { Box, Button, Container, Divider, Text } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import { useQuery } from '@apollo/client'
 import { PostDetailDocument } from './PostDetail.generate.graphql'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const PostDetail: NextPage = () => {
   const router = useRouter()
@@ -15,10 +16,20 @@ const PostDetail: NextPage = () => {
   })
   return (
     <Container maxW={500} w={'full'} mx={'auto'}>
-      <Text>{data?.post.title}</Text>
-      <Text>{data?.post.content}</Text>
-      <Text>{data?.post.author.name}</Text>
-      <Text>{data?.post.author.email}</Text>
+      <Text fontWeight={'bold'} fontSize={'xl'}>
+        {data?.post.title}
+      </Text>
+      <Divider />
+      <Text mt={5}>{data?.post.content}</Text>
+      <Box mt={10}>
+        <Text>{data?.post.author.name}</Text>
+        <Text>{data?.post.author.email}</Text>
+      </Box>
+      <Link passHref href={'/'}>
+        <Button mt={5} w={'full'} as={'a'}>
+          一覧に戻る
+        </Button>
+      </Link>
     </Container>
   )
 }
