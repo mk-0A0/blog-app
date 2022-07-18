5545fb786984702dfcda15eba8ec9cfdc119ee25
momoka <moowfaw@gmail.com>
2022-07-18 18:01:49 +0900

    🔧投稿した記事に遷移

--- frontend/src/pages/post/complete/index.tsx
+++ frontend/src/pages/post/complete/index.tsx
@@ -1,16 +1,19 @@
 import { Button, Center, Container } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const PostComplete: NextPage = () => {
+  const router = useRouter()
+  const { query } = router
   return (
     <Container maxW={500} w={'full'} mx={'auto'}>
       <Center fontSize={'lg'} fontWeight={'bold'}>
         記事を投稿しました
       </Center>
       {/*TODO:UXを考える*/}
-      <Link passHref href={`/`}>
-        <Button w={'full'} mt={10}>
+      <Link passHref href={`/${query.uuid}`}>
+        <Button w={'full'} mt={10} as={'a'}>
           記事を見る
         </Button>
       </Link>
