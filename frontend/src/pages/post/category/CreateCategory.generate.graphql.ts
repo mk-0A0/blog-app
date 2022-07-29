41684c85bb9ac5ffb2324f5d3de5a0ec9833c78e
momoka <moowfaw@gmail.com>
2022-07-29 21:36:21 +0900

    💄format

--- frontend/src/pages/post/category/CreateCategory.generate.graphql.ts
+++ frontend/src/pages/post/category/CreateCategory.generate.graphql.ts
@@ -1,12 +1,66 @@
-import * as Types from '../../../type/__generate__/graphql';
+import * as Types from '../../../type/__generate__/graphql'
 
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
 export type CreateCategoryMutationVariables = Types.Exact<{
-  input: Types.CreateCategoryInput;
-}>;
+  input: Types.CreateCategoryInput
+}>
 
+export type CreateCategoryMutation = {
+  __typename?: 'Mutation'
+  createCategory: { __typename?: 'Category'; name: string }
+}
 
-export type CreateCategoryMutation = { __typename?: 'Mutation', createCategory: { __typename?: 'Category', name: string } };
-
-
-export const CreateCategoryDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateCategory"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"input"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"CreateCategoryInput"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createCategory"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"input"},"value":{"kind":"Variable","name":{"kind":"Name","value":"input"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]} as unknown as DocumentNode<CreateCategoryMutation, CreateCategoryMutationVariables>;
+export const CreateCategoryDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'mutation',
+      name: { kind: 'Name', value: 'CreateCategory' },
+      variableDefinitions: [
+        {
+          kind: 'VariableDefinition',
+          variable: {
+            kind: 'Variable',
+            name: { kind: 'Name', value: 'input' },
+          },
+          type: {
+            kind: 'NonNullType',
+            type: {
+              kind: 'NamedType',
+              name: { kind: 'Name', value: 'CreateCategoryInput' },
+            },
+          },
+        },
+      ],
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'createCategory' },
+            arguments: [
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'input' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'input' },
+                },
+              },
+            ],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<
+  CreateCategoryMutation,
+  CreateCategoryMutationVariables
+>
--- frontend/src/type/__generate__/graphql.ts
+++ frontend/src/type/__generate__/graphql.ts
@@ -1,259 +1,255 @@
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Maybe<T> = T | null
+export type InputMaybe<T> = Maybe<T>
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K]
+}
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>
+}
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>
+}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-};
+  ID: string
+  String: string
+  Boolean: boolean
+  Int: number
+  Float: number
+}
 
 export type Category = {
-  __typename?: 'Category';
+  __typename?: 'Category'
   /** id */
-  id: Scalars['ID'];
+  id: Scalars['ID']
   /** カテゴリー名 */
-  name: Scalars['String'];
+  name: Scalars['String']
   /** カテゴリーに紐付いている記事 */
-  posts: Array<Post>;
+  posts: Array<Post>
   /** uuid */
-  uuid: Scalars['String'];
-};
+  uuid: Scalars['String']
+}
 
 export type CategoryCount = {
-  __typename?: 'CategoryCount';
-  posts: Scalars['Int'];
-};
+  __typename?: 'CategoryCount'
+  posts: Scalars['Int']
+}
 
 export type CategoryEntity = {
-  __typename?: 'CategoryEntity';
-  _count: CategoryCount;
+  __typename?: 'CategoryEntity'
+  _count: CategoryCount
   /** id */
-  id: Scalars['ID'];
+  id: Scalars['ID']
   /** カテゴリー名 */
-  name: Scalars['String'];
+  name: Scalars['String']
   /** カテゴリーに紐付いている記事 */
-  posts?: Maybe<Array<PostEntity>>;
+  posts?: Maybe<Array<PostEntity>>
   /** uuid */
-  uuid: Scalars['String'];
-};
+  uuid: Scalars['String']
+}
 
 export type CreateCategoryInput = {
   /** カテゴリー名 */
-  name: Scalars['String'];
-};
+  name: Scalars['String']
+}
 
 export type CreatePostInput = {
   /** 記事に付けるカテゴリー */
-  categoryUuids?: InputMaybe<Array<Scalars['String']>>;
+  categoryUuids?: InputMaybe<Array<Scalars['String']>>
   /** 記事の本文 */
-  content: Scalars['String'];
+  content: Scalars['String']
   /** 記事を公開するか */
-  isPublished?: InputMaybe<Scalars['Boolean']>;
+  isPublished?: InputMaybe<Scalars['Boolean']>
   /** 記事のタイトル */
-  title: Scalars['String'];
-};
+  title: Scalars['String']
+}
 
 export type CreateUserInput = {
   /** ユーザーメールアドレス */
-  email: Scalars['String'];
+  email: Scalars['String']
   /** ユーザー名 */
-  name: Scalars['String'];
+  name: Scalars['String']
   /** ユーザーパスワード */
-  password: Scalars['String'];
-};
+  password: Scalars['String']
+}
 
 export type LoginUserInput = {
   /** ユーザーメールアドレス */
-  email: Scalars['String'];
+  email: Scalars['String']
   /** ユーザーパスワード */
-  password: Scalars['String'];
-};
+  password: Scalars['String']
+}
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: 'Mutation'
   /** カテゴリーを作成する */
-  createCategory: Category;
+  createCategory: Category
   /** 記事を作成する */
-  createPost: Post;
-  createUser: User;
+  createPost: Post
+  createUser: User
   /** カテゴリーを削除する */
-  deleteCategory: Category;
+  deleteCategory: Category
   /** 記事を削除する */
-  deletePost: Post;
-  login: Token;
+  deletePost: Post
+  login: Token
   /** カテゴリーを更新する */
-  updateCategory: Category;
+  updateCategory: Category
   /** 記事を更新する */
-  updatePost: Post;
-};
-
+  updatePost: Post
+}
 
 export type MutationCreateCategoryArgs = {
-  input: CreateCategoryInput;
-};
-
+  input: CreateCategoryInput
+}
 
 export type MutationCreatePostArgs = {
-  input: CreatePostInput;
-};
-
+  input: CreatePostInput
+}
 
 export type MutationCreateUserArgs = {
-  createUserInput: CreateUserInput;
-};
-
+  createUserInput: CreateUserInput
+}
 
 export type MutationDeleteCategoryArgs = {
-  uuid: Scalars['String'];
-};
-
+  uuid: Scalars['String']
+}
 
 export type MutationDeletePostArgs = {
-  uuid: Scalars['String'];
-};
-
+  uuid: Scalars['String']
+}
 
 export type MutationLoginArgs = {
-  loginUserInput: LoginUserInput;
-};
-
+  loginUserInput: LoginUserInput
+}
 
 export type MutationUpdateCategoryArgs = {
-  input: UpdateCategoryInput;
-  uuid: Scalars['String'];
-};
-
+  input: UpdateCategoryInput
+  uuid: Scalars['String']
+}
 
 export type MutationUpdatePostArgs = {
-  input: UpdatePostInput;
-  uuid: Scalars['String'];
-};
+  input: UpdatePostInput
+  uuid: Scalars['String']
+}
 
 export type Post = {
-  __typename?: 'Post';
+  __typename?: 'Post'
   /** 投稿者 */
-  author: User;
+  author: User
   /** 記事の作者のID */
-  authorId: Scalars['Int'];
+  authorId: Scalars['Int']
   /** カテゴリー */
-  categories: Array<Category>;
+  categories: Array<Category>
   /** 記事の本文 */
-  content: Scalars['String'];
+  content: Scalars['String']
   /** id */
-  id: Scalars['ID'];
+  id: Scalars['ID']
   /** 記事が公開しているかどうか */
-  isPublished: Scalars['Boolean'];
+  isPublished: Scalars['Boolean']
   /** 記事のタイトル */
-  title: Scalars['String'];
+  title: Scalars['String']
   /** uuid */
-  uuid: Scalars['String'];
-};
+  uuid: Scalars['String']
+}
 
 export type PostCount = {
-  __typename?: 'PostCount';
-  categories: Scalars['Int'];
-};
+  __typename?: 'PostCount'
+  categories: Scalars['Int']
+}
 
 export type PostEntity = {
-  __typename?: 'PostEntity';
-  _count: PostCount;
+  __typename?: 'PostEntity'
+  _count: PostCount
   /** 記事の作者 */
-  author: UserEntity;
+  author: UserEntity
   /** 記事の作者のID */
-  authorId: Scalars['Int'];
+  authorId: Scalars['Int']
   /** 記事のカテゴリー */
-  categories?: Maybe<Array<CategoryEntity>>;
+  categories?: Maybe<Array<CategoryEntity>>
   /** 記事の本文 */
-  content: Scalars['String'];
+  content: Scalars['String']
   /** id */
-  id: Scalars['ID'];
+  id: Scalars['ID']
   /** 記事が公開しているかどうか */
-  isPublished: Scalars['Boolean'];
+  isPublished: Scalars['Boolean']
   /** 記事のタイトル */
-  title: Scalars['String'];
+  title: Scalars['String']
   /** uuid */
-  uuid: Scalars['String'];
-};
+  uuid: Scalars['String']
+}
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: 'Query'
   /** カテゴリーを複数取得する */
-  categories: Array<Category>;
+  categories: Array<Category>
   /** uuidからカテゴリーを取得する */
-  category: Category;
+  category: Category
   /** 記事を取得する */
-  post: Post;
+  post: Post
   /** 記事を複数取得する */
-  posts: Array<Post>;
-};
-
+  posts: Array<Post>
+}
 
 export type QueryCategoryArgs = {
-  uuid: Scalars['String'];
-};
-
+  uuid: Scalars['String']
+}
 
 export type QueryPostArgs = {
-  uuid: Scalars['String'];
-};
+  uuid: Scalars['String']
+}
 
 export type Token = {
-  __typename?: 'Token';
+  __typename?: 'Token'
   /** JWTトークン */
-  token: Scalars['String'];
-};
+  token: Scalars['String']
+}
 
 export type UpdateCategoryInput = {
   /** カテゴリー名 */
-  name?: InputMaybe<Scalars['String']>;
-};
+  name?: InputMaybe<Scalars['String']>
+}
 
 export type UpdatePostInput = {
   /** 記事に付けるカテゴリー */
-  categoryUuids?: InputMaybe<Array<Scalars['String']>>;
+  categoryUuids?: InputMaybe<Array<Scalars['String']>>
   /** 記事の本文 */
-  content?: InputMaybe<Scalars['String']>;
+  content?: InputMaybe<Scalars['String']>
   /** 記事を公開するか */
-  isPublished?: InputMaybe<Scalars['Boolean']>;
+  isPublished?: InputMaybe<Scalars['Boolean']>
   /** 記事のタイトル */
-  title?: InputMaybe<Scalars['String']>;
-};
+  title?: InputMaybe<Scalars['String']>
+}
 
 export type User = {
-  __typename?: 'User';
+  __typename?: 'User'
   /** メールアドレス */
-  email: Scalars['String'];
+  email: Scalars['String']
   /** id */
-  id: Scalars['ID'];
+  id: Scalars['ID']
   /** 名前 */
-  name: Scalars['String'];
+  name: Scalars['String']
   /** 投稿 */
-  posts?: Maybe<Array<Post>>;
+  posts?: Maybe<Array<Post>>
   /** uuid */
-  uuid: Scalars['String'];
-};
+  uuid: Scalars['String']
+}
 
 export type UserCount = {
-  __typename?: 'UserCount';
-  posts: Scalars['Int'];
-};
+  __typename?: 'UserCount'
+  posts: Scalars['Int']
+}
 
 export type UserEntity = {
-  __typename?: 'UserEntity';
-  _count: UserCount;
+  __typename?: 'UserEntity'
+  _count: UserCount
   /** メールアドレス */
-  email: Scalars['String'];
+  email: Scalars['String']
   /** id */
-  id: Scalars['ID'];
+  id: Scalars['ID']
   /** 名前 */
-  name: Scalars['String'];
+  name: Scalars['String']
   /** ユーザーが投稿した記事 */
-  posts?: Maybe<Array<PostEntity>>;
+  posts?: Maybe<Array<PostEntity>>
   /** uuid */
-  uuid: Scalars['String'];
-};
+  uuid: Scalars['String']
+}
