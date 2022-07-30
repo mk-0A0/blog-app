4b249d3e716e0106f4170a4a07ad9cfa65c5bfac
momoka <moowfaw@gmail.com>
2022-07-30 22:59:10 +0900

    ✨delete category

---
+++ frontend/src/pages/post/category/[category_uuid]/DeleteCategory.generate.graphql.ts
@@ -0,0 +1,12 @@
+import * as Types from '../../../../type/__generate__/graphql';
+
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type DeleteCategoryMutationVariables = Types.Exact<{
+  uuid: Types.Scalars['String'];
+}>;
+
+
+export type DeleteCategoryMutation = { __typename?: 'Mutation', deleteCategory: { __typename?: 'Category', id: string, name: string, uuid: string } };
+
+
+export const DeleteCategoryDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"DeleteCategory"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"uuid"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"deleteCategory"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"uuid"},"value":{"kind":"Variable","name":{"kind":"Name","value":"uuid"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"uuid"}}]}}]}}]} as unknown as DocumentNode<DeleteCategoryMutation, DeleteCategoryMutationVariables>;
--- frontend/src/pages/post/category/[category_uuid]/index.tsx
+++ frontend/src/pages/post/category/[category_uuid]/index.tsx
@@ -4,11 +4,38 @@ import {
   FormControl,
   FormLabel,
   Input,
+  useToast,
   VStack,
 } from '@chakra-ui/react'
 import { NextPage } from 'next'
+import { useMutation } from '@apollo/client'
+import { DeleteCategoryDocument } from './DeleteCategory.generate.graphql'
+import { useRouter } from 'next/router'
 
 const CategoryDetail: NextPage = () => {
+  const { push, query } = useRouter()
+  const toast = useToast()
+
+  /**
+   * delete
+   */
+  const [deleteCategory, { loading: deleteLoading }] = useMutation(
+    DeleteCategoryDocument
+  )
+  const onDelete = async () => {
+    try {
+      await deleteCategory({
+        variables: {
+          uuid: `${query.category_uuid}`,
+        },
+      })
+      toast({ status: 'success', title: 'カテゴリを削除しました' })
+      push('/post/category')
+    } catch (e) {
+      toast({ status: 'error', title: 'カテゴリを削除できませんでした' })
+    }
+  }
+
   return (
     <Container maxW={500} w={'full'} mx={'auto'}>
       <VStack gap={5}>
@@ -17,7 +44,13 @@ const CategoryDetail: NextPage = () => {
           <Input />
         </FormControl>
         <Button w={'full'}>更新</Button>
-        <Button w={'full'} variant={'ghost'} colorScheme={'red'}>
+        <Button
+          w={'full'}
+          variant={'ghost'}
+          colorScheme={'red'}
+          isLoading={deleteLoading}
+          onClick={() => onDelete()}
+        >
           削除
         </Button>
       </VStack>
