2b9a0e2372c056a3ec0e2db04ddeeccc74da42e0
momoka <moowfaw@gmail.com>
2022-07-30 22:58:33 +0900

    💄format

--- frontend/src/pages/[post_uuid]/edit/PostDelete.generate.graphql.ts
+++ frontend/src/pages/[post_uuid]/edit/PostDelete.generate.graphql.ts
@@ -1,60 +1,12 @@
-import * as Types from '../../../type/__generate__/graphql'
+import * as Types from '../../../type/__generate__/graphql';
 
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type DeletePostMutationVariables = Types.Exact<{
-  uuid: Types.Scalars['String']
-}>
+  uuid: Types.Scalars['String'];
+}>;
 
-export type DeletePostMutation = {
-  __typename?: 'Mutation'
-  deletePost: { __typename?: 'Post'; uuid: string }
-}
 
-export const DeletePostDocument = {
-  kind: 'Document',
-  definitions: [
-    {
-      kind: 'OperationDefinition',
-      operation: 'mutation',
-      name: { kind: 'Name', value: 'deletePost' },
-      variableDefinitions: [
-        {
-          kind: 'VariableDefinition',
-          variable: { kind: 'Variable', name: { kind: 'Name', value: 'uuid' } },
-          type: {
-            kind: 'NonNullType',
-            type: {
-              kind: 'NamedType',
-              name: { kind: 'Name', value: 'String' },
-            },
-          },
-        },
-      ],
-      selectionSet: {
-        kind: 'SelectionSet',
-        selections: [
-          {
-            kind: 'Field',
-            name: { kind: 'Name', value: 'deletePost' },
-            arguments: [
-              {
-                kind: 'Argument',
-                name: { kind: 'Name', value: 'uuid' },
-                value: {
-                  kind: 'Variable',
-                  name: { kind: 'Name', value: 'uuid' },
-                },
-              },
-            ],
-            selectionSet: {
-              kind: 'SelectionSet',
-              selections: [
-                { kind: 'Field', name: { kind: 'Name', value: 'uuid' } },
-              ],
-            },
-          },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode<DeletePostMutation, DeletePostMutationVariables>
+export type DeletePostMutation = { __typename?: 'Mutation', deletePost: { __typename?: 'Post', uuid: string } };
+
+
+export const DeletePostDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"deletePost"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"uuid"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"deletePost"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"uuid"},"value":{"kind":"Variable","name":{"kind":"Name","value":"uuid"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"uuid"}}]}}]}}]} as unknown as DocumentNode<DeletePostMutation, DeletePostMutationVariables>;
