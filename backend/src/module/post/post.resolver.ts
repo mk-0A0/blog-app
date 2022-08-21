b35cec54ca202bccc31cded3ed8d41f9ddf83b44
hisho <hisho.web@gmail.com>
2022-08-22 08:59:38 +0900

    ✨ 投稿にUserGuardを追加

--- backend/src/module/post/post.resolver.ts
+++ backend/src/module/post/post.resolver.ts
@@ -4,6 +4,10 @@ import { Post } from './entities/post.entity';
 import { CreatePostInput } from './dto/create-post.input';
 import { UserService } from '../user/user.service';
 import { UpdatePostInput } from './dto/update-post.input';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
+import { CurrentUser } from '../../decorators/CurrentUser';
+import { User } from '../user/entities/user.entity';
 
 @Resolver(() => Post)
 export class PostResolver {
@@ -12,15 +16,18 @@ export class PostResolver {
     private readonly user: UserService,
   ) {}
 
-  //TODO JWTGuardを作ってuserを認証ユーザーに変える
   @Mutation(() => Post, { description: '記事を作成する' })
-  async createPost(@Args('input') createPostInput: CreatePostInput) {
-    const user = await this.user.getSeedUser();
+  @UseGuards(JwtAuthGuard)
+  async createPost(
+    @Args('input') createPostInput: CreatePostInput,
+    @CurrentUser() user: User,
+  ) {
     return this.postService.create(user, createPostInput);
   }
 
-  //TODO JWTGuardを作ってuserを認証ユーザーに変える
+  //TODO 現在ログインしているユーザーの投稿のみ更新できるようにする
   @Mutation(() => Post, { description: '記事を更新する' })
+  @UseGuards(JwtAuthGuard)
   async updatePost(
     @Args('input') updatePostInput: UpdatePostInput,
     @Args('uuid') uuid: string,
@@ -29,21 +36,24 @@ export class PostResolver {
     return this.postService.update(uuid, updatePostInput);
   }
 
-  //TODO JWTGuardを作ってuserを認証ユーザーに変える
+  //TODO 現在ログインしているユーザーの投稿のみ削除できるようにする
   @Mutation(() => Post, { description: '記事を削除する' })
+  @UseGuards(JwtAuthGuard)
   deletePost(@Args('uuid') uuid: string) {
     return this.postService.delete(uuid);
   }
 
-  //TODO JWTGuardを作ってuserを認証ユーザーに変える
+  //TODO 名前を変える
   @Query(() => [Post], { description: '記事を複数取得する' })
+  @UseGuards(JwtAuthGuard)
   async posts() {
     const user = await this.user.getSeedUser();
     return this.postService.findAll(user);
   }
 
-  //TODO JWTGuardを作ってuserを認証ユーザーに変える
+  //TODO 名前を変える
   @Query(() => Post, { description: '記事を取得する' })
+  @UseGuards(JwtAuthGuard)
   async post(@Args('uuid') uuid: string) {
     const user = await this.user.getSeedUser();
     return this.postService.findOne(user, uuid);
