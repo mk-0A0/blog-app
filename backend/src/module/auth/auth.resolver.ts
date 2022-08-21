585798ed92e338a1814bd449c7a153759e6d373b
hisho <hisho.web@gmail.com>
2022-08-21 18:30:44 +0900

    🔥 remove userGuard for login

--- backend/src/module/auth/auth.resolver.ts
+++ backend/src/module/auth/auth.resolver.ts
@@ -4,14 +4,12 @@ import { User } from '../user/entities/user.entity';
 import { AuthService } from './auth.service';
 import { LoginUserInput } from './dto/login-user.input';
 import { Token } from './entities/token.entity';
-import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
   @Mutation(() => Token)
-  @UseGuards(LocalAuthGuard)
   async login(
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
